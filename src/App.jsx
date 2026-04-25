fe82d8de0f4bca046dd65bfa128099f60a735372
Marcos Silva <1609.marcos.silva@gmail.com>
2026-04-25 14:35:28 -0300

    JWT Running

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,27 @@ import Register from "./pages/Register";
 import Welcome from "./pages/Welcome";
 
 export default function App() {
+  const token = localStorage.getItem("lyra_token");
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/welcome" element={<Welcome />} />
-      <Route path="/" element={<Navigate to="/welcome" replace />} />
-      <Route path="*" element={<Navigate to="/welcome" replace />} />
+
+      <Route
+        path="/welcome"
+        element={token ? <Welcome /> : <Navigate to="/login" replace />}
+      />
+
+      <Route
+        path="/"
+        element={<Navigate to={token ? "/welcome" : "/login"} replace />}
+      />
+
+      <Route
+        path="*"
+        element={<Navigate to={token ? "/welcome" : "/login"} replace />}
+      />
     </Routes>
   );
 }
