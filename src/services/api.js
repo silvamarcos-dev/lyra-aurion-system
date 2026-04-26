eba6ad293af095367f452eb949635abbe511f87f
Marcos Silva <1609.marcos.silva@gmail.com>
2026-04-25 22:30:12 -0300

    frontend running

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.146:8000",
+  baseURL: "https://accommodations-quantities-pets-recommendation.trycloudflare.com",
 });
 
 api.interceptors.request.use((config) => {
