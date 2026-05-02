ab05bcaf0c5ced2349781671a6877f39412a91ea
Marcos Silva <1609.marcos.silva@gmail.com>
2026-05-02 13:06:54 -0300

    lyra running

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://christ-hundreds-achieved-pose.trycloudflare.com",
+  baseURL: "https://response-induction-hindu-procedures.trycloudflare.com",
 });
 
 api.interceptors.request.use((config) => {
